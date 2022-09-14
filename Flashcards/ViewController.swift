4686ba28ca104efd68fa2eafa42dcdc11dec1283
jejack5203 <109096014+jejack5203@users.noreply.github.com>
2022-09-13 22:10:17 -0400

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Jodie Jackson on 9/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
